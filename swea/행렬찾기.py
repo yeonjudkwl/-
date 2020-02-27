aae6d38d1410dfd28d9084e69ae29a334f51cb1f
yeonjudkwl <hyj9785@likelion.org>
2020-02-27 14:41:54 +0900

    200227

--- swea/행렬찾기.py
+++ swea/행렬찾기.py
@@ -10,18 +10,24 @@ for tc in range(T):
     for i in range(N):
         for j in range(N):
             if data[i][j] != 0:
+                #i, j는 시작 좌표
                 x, y = i, j
+                #x, y로 끝 좌표 찾기
                 while data[x][j] != 0:
                     x += 1
                 while data[i][y] != 0:
                     y += 1
+                #몇 행, 몇 열의 직사각형인지 저장
                 temp.append((x-i, y-j))
 
+                #찾은 직사각형은 0으로 초기화
                 for ii in range(i, x+1):
                     for jj in range(j, y+1):
                         data[ii][jj] = 0
+                #찾은 직사각형 갯수
                 cnt += 1
 
+    #직사각형의 (행*열)이 작은 좌표부터 정렬
     for i in range(0, len(temp)-1):
         min = i		
         for j in range(i+1, len(temp)):
@@ -31,5 +37,6 @@ for tc in range(T):
 
     print("#{} {}".format(tc+1, cnt), end=" ")
     for t in temp:
+        #tuple unpacking
         print(*t, end=' ')
     print()

141a5407cef75794d9a60bc9a3a750c2038c8262
yeonjudkwl <hyj9785@likelion.org>
2020-05-22 14:15:24 +0900

    200521

--- swea/연산.py
+++ swea/연산.py
@@ -3,16 +3,20 @@ from collections import deque
 def bfs(n):
     cnt = 0
     queue = deque()
+    # 연산 했던 num 체크 할 list
     lists = []
     queue.append((n, cnt))
     while queue:
         num, count = queue.popleft()
+        # 연산 했었으면 continue
         if num in lists: continue
+        # num이 음수면 continue
         if int(num) < 0: continue
-        # print(num)
+        # 연산 할 num을 lists에 추가
         lists.append(num)
 
         for w in ['+1', '-1', '*2', '-10']:
+            # 각 연산 시행
             rst = eval(str(num)+w)
             if rst != int(M):
                 queue.append((rst, count+1))
@@ -24,6 +28,5 @@ sys.stdin = open("연산.txt")
 
 for tc in range(int(input())):
     N, M = input().split()
-    visited = [0 for _ in range(4)]
 
     print("#{} {}".format(tc+1, bfs(N)))
